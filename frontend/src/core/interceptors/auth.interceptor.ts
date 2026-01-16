ee1e6b313f39e237d75a0af417af34b0b58c39a5
DiegoMillanOrtega <diegomillan1005@gmail.com>
2026-01-16 18:20:40 -0500

    feat: se captura el estado 401

--- frontend/src/core/interceptors/auth.interceptor.ts
+++ frontend/src/core/interceptors/auth.interceptor.ts
@@ -1,9 +1,22 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError, throwError } from "rxjs";
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+    const router = inject(Router);
     const clonedRequest = req.clone({
         withCredentials: true,
     });
 
-    return next(clonedRequest);
+    return next(clonedRequest).pipe(
+        catchError((error: HttpErrorResponse) => {
+            if (error.status === 401) {
+                console.warn(error.message);
+                
+                router.navigate(['/auth/login']);
+            }
+            return throwError(error);
+        })
+    );
 };
